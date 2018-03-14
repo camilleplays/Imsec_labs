ec3c76e47c53eb2b4151daf21f3acc38ace439fa
camille <plays.camille@gmail.com>
2018-03-14 17:10:54 +0100

    pic

--- TpBiometry/public/Exercices/exercice1.m
+++ TpBiometry/public/Exercices/exercice1.m
@@ -6,40 +6,27 @@
 %      Part A      %
 %%%%%%%%%%%%%%%%%%%%
 
-
-addpath \\datas\teaching\courses\image\Tpbiometry\public\Matlab
-train_A = loadImagesInDirectory('\\datas\teaching\courses\image\Tpbiometry\public\Images\train_A\');
+addpath ..\Matlab
+train_A = loadImagesInDirectory('..\Images\train_A\');
 [Means, SpaceA, Eigenvalues] = buildSpace(train_A);
-
-% what is the maximum size of the eigenspace ie how many directions of
-% variability can we estimate?
-
-%there are as many directions as there are eigenvalues: 100
-
 
 %%%%%%%%%%%%%%%%%%%%
 %      Part B      %
 %%%%%%%%%%%%%%%%%%%%
-Cumulated = cumsum(Eigenvalues);
-plot(Cumulated);
 
-%as it is cumulative, the curve is growing. However, as we add eigenvalues,
-%they contain less and less information. This is why the curve is similar 
-%to a logarithmic function.
+Cumulated = cumsum(Eigenvalues);
+plot(Cumulated)
+xlabel('number of eigenvalues')
+ylabel('cumulated sum')
 
 %%%%%%%%%%%%%%%%%%%%
 %      Part C      %
 %%%%%%%%%%%%%%%%%%%%
 
-figureHandle1 = approximateImage('\\datas\teaching\courses\image\Tpbiometry\public\Images\train_A\s1_1.jpg',Means,SpaceA,12);
-figureHandle2 = approximateImage('\\datas\teaching\courses\image\Tpbiometry\public\Images\train_A\s1_1.jpg',Means,SpaceA,40);
-figureHandle3 = approximateImage('\\datas\teaching\courses\image\Tpbiometry\public\Images\train_A\s1_1.jpg',Means,SpaceA,75);
-figureHandle4 = approximateImage('\\datas\teaching\courses\image\Tpbiometry\public\Images\train_A\s1_1.jpg',Means,SpaceA,100);
-
-% CAN WE REBUILD IT PERFECTLY ?
-% Yes we can rebuild nearly perfectly the image using all the EigenValues,
-% since we have all the informations that we need in all of the
-% eigenvalues.
+figureHandle1 = approximateImage('..\Images\train_A\s1_1.jpg',Means,SpaceA,12);
+figureHandle2 = approximateImage('..\Images\train_A\s1_1.jpg',Means,SpaceA,40);
+figureHandle3 = approximateImage('..\Images\train_A\s1_1.jpg',Means,SpaceA,75);
+figureHandle4 = approximateImage('..\Images\train_A\s1_1.jpg',Means,SpaceA,100);
 
 %%%%%%%%%%
 % Part D %
