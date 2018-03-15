114fb522294eb553618e7ef46272a6989c2763fd
camille <plays.camille@gmail.com>
2018-03-15 14:42:24 +0100

    ex4

--- TpBiometry/public/Exercices/exercice3.m
+++ TpBiometry/public/Exercices/exercice3.m
@@ -12,3 +12,38 @@ test_projection = projectImages(test_A, Means, SpaceA);
 figure;
 hold on;
 plot(XClients,YClients,'b',XImpostors,(YImpostors/19),'r');
+
+%%%%%%%%%%%%%%%%%%%%
+%      Part B      %
+%%%%%%%%%%%%%%%%%%%%
+
+[FalseRejectionRates, FalseAcceptanceRates] = computeVerificationRates(DistancesClients,DistancesImpostors);
+figure;
+subplot(1,2,1)
+plot(FalseRejectionRates,'b');
+
+subplot(1,2,2)
+plot(FalseAcceptanceRates,'r');
+
+%this technology is used when we have various threshold settings. 
+
+%%%%%%%%%%%%%%%%%%%%
+%      Part C      %
+%%%%%%%%%%%%%%%%%%%%
+
+EqualErrorRate = zeros(1,100)
+
+for i = 1:100
+    [DistancesClients, DistancesImpostors] = verify(model_projection,test_projection,i);
+    EqualErrorRate(i)=computeEER(DistancesClients, DistancesImpostors);
+end
+
+hold off
+figure();
+plot(EqualErrorRate)
+xlabel('number of eigenfaces')
+ylabel('equal error rate')
+
+
+
+
